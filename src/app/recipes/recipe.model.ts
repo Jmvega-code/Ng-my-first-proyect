411f28ae648e01d9912a8af89bc9648195839b35
jmvega <jmvega@bebeyond.es>
2020-09-15 14:27:41 +0200

    The basics

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+import { strict } from "assert";
+
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name:string, descr: string, imagePath: string) {
+    this.name = name;
+    this.description = descr;
+    this.imagePath = imagePath;
+  }
+}
